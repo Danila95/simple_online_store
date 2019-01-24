de3ee558bc6731c697720189817eb14878f9322a
Danila <Danila00000@Gmail.com>
2019-01-24 08:46:28 +0700

    Complited lesson1.3

--- simple_online_store/mainApp/admin.py
+++ simple_online_store/mainApp/admin.py
@@ -1,4 +1,24 @@
 from django.contrib import admin
 from . models import *
 
-admin.site.register(Subscribers)
+class SubscriberAdmin (admin.ModelAdmin):
+	# выведем поля в таблице в моделе Subscriber в админке
+    # list_display = ["name", "email"]
+	# Тоже что и выше, только выводится в цикле (выводит все поля, какие есть, даже id)
+    list_display = [field.name for field in Subscriber._meta.fields]
+    list_filter = ['name',] # позволяет фильтровать данные по столбцу
+    search_fields = ['name', 'email'] # позволяет фильтровать данные с помощью input search
+
+    fields = ["email"] # отображать только поле email в редактировании записи
+
+    # exclude = ["email"] # исключить поле email (исключает возможность редактирования данного поля)
+	# inlines = [FieldMappingInline]
+
+	# #list_filter = ('report_data',)
+	# search_fields = ['category', 'subCategory', 'suggestKeyword']
+
+    class Meta:
+        model = Subscriber
+
+# регистрируем модель Subscriber, которую мы импорттировали из models.py и рег. класс SubscriberAdmin
+admin.site.register(Subscriber, SubscriberAdmin)
--- simple_online_store/mainApp/models.py
+++ simple_online_store/mainApp/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 
-class Subscribers(models.Model):
+class Subscriber(models.Model):
 	email = models.EmailField()
 	name = models.CharField(max_length=128)
+
+# функция, которая выводит нужные значения из поля БД и выводит ее в одну строку в админке
+	def __str__(self):
+			return 'id: %s Пользователь: %s email: %s' % (self.id, self.name, self.email)
+
+
+	# def __str__(self):
+	# 	try:
+	# 		return self.user.username
+	# 	except:
+	# 		return '%s' % self.user.id
+
+	# класс который обозначает названия таблиц в единственном и множественном числе в админке
+	class Meta:
+		verbose_name = 'MySubscriber'
+		verbose_name_plural = 'A lot of Subscribers'
