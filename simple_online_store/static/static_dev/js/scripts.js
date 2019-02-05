ce44c109198fa1e6e17ed1ee0da965baa4c31db0
Danila <Danila00000@Gmail.com>
2019-02-05 10:34:46 +0700

    Complited Lesson1.8.3

--- simple_online_store/static/static_dev/js/scripts.js
+++ simple_online_store/static/static_dev/js/scripts.js
@@ -3,17 +3,22 @@ $(document).ready(function(){
     console.log(form);
     form.on('submit', function(e) {
         e.preventDefault(); // отменяем работу функции по умолчанию (в этом случае форма не будет отправляться)
-        console.log('123');
         let nmb = $('#number').val();
-        console.log(nmb);
+        console.log('nmb = ' + nmb);
         let submit_btn = $('#submit_btn');
         let product_id = submit_btn.data('product_id');
         let product_name = submit_btn.data('name');
         let product_price = submit_btn.data("price");
         let product_currency = submit_btn.data("currency");
-        console.log(product_id);
-        console.log(product_name);
-        console.log(product_price);
+
+        // подсчитаем общую цену за один конкретный товар (если берется несколько штук)
+        product_price = product_price.replace( /,/g, "." ); // JS обозначает вещественные числа точкой
+        let total_price = parseFloat(nmb) * parseFloat(product_price);
+        // подготовления числа для отображения в списке корзины
+        total_price = total_price.toFixed(2);
+        total_price = String(total_price);
+        total_price = total_price.replace( ".", "," );
+        console.log(total_price);
 
         let data = {};
         data.product_id = product_id;
@@ -32,15 +37,20 @@ $(document).ready(function(){
                 console.log(data.products_total_nmb);
                 if (data.products_total_nmb)
                     $('#basket_total_nmb').text('('+data.products_total_nmb+")");
+                    console.log(data.products);
+                    $('.basket-items ul').html("");
+                    $.each(data.products, function() {
+                        $('.basket-items ul').append('<li>'+product_name+', ' + nmb + 'шт. ' + 'по ' + product_price + ' '+ product_currency + ' ' +
+                            '<a class="delete-item">X</a>'+
+                            '<p>Всего: ' + total_price + ' ' + product_currency + '</p></li>');
+                    });
             },
             error: function () {
                 console.log('error')
             }
         });
 
-        $('.basket-items ul').append('<li>'+product_name+', ' + nmb + 'шт. ' + 'по ' + product_price + ' '+ product_currency + ' ' +
-            '<a class="delete-item">X</a>'+
-            '</li>');
+
     });
 
     function showingBasket() {
