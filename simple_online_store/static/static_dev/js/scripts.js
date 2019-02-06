7bc062f109e54a7ff035c4d76e8e2c651ba3c031
Danila <Danila00000@Gmail.com>
2019-02-06 15:07:49 +0700

    Complited Lesson1.8.4

--- simple_online_store/static/static_dev/js/scripts.js
+++ simple_online_store/static/static_dev/js/scripts.js
@@ -1,6 +1,46 @@
 $(document).ready(function(){
     let form = $('#form_buying_product');
     console.log(form);
+
+    function basketUpdating(product_id, nmb, is_delete) {
+        let data = {};
+        data.product_id = product_id;
+        data.nmb = nmb;
+        var csrf_token = $('#form_buying_product [name="csrfmiddlewaretoken"]').val();
+        data["csrfmiddlewaretoken"] = csrf_token;
+
+        if (is_delete){
+			data["is_delete"] = true;
+		}
+
+        let url = form.attr("action");
+        console.log(data);
+        $.ajax({
+            url: url,
+            type: 'POST',
+            data: data,
+            cache: true,
+            success: function (data) {
+                console.log('OK');
+                console.log(data.products_total_nmb);
+                if (data.products_total_nmb || data.products_total_nmb == 0) {
+                    $('#basket_total_nmb').text('('+data.products_total_nmb+")");
+                    console.log(data.products);
+                    $('.basket-items ul').html("");
+                    $.each(data.products, function(k, v) {
+                        $('.basket-items ul').append('<li>'+v.name+', ' + v.nmb + 'шт. ' + 'по ' + v.price_per_item + ' '+ v.currency + ' ' +
+                            '<a class="delete-item" data-product_id="'+ v.id +'">X</a>'+
+                            '<p>Всего: ' + v.total_price + ' ' + v.currency + '</p></li>');
+                    });
+                }
+            },
+            error: function () {
+                console.log('error')
+            }
+        });
+
+    }
+
     form.on('submit', function(e) {
         e.preventDefault(); // отменяем работу функции по умолчанию (в этом случае форма не будет отправляться)
         let nmb = $('#number').val();
@@ -20,35 +60,7 @@ $(document).ready(function(){
         total_price = total_price.replace( ".", "," );
         console.log(total_price);
 
-        let data = {};
-        data.product_id = product_id;
-        data.nmb = nmb;
-        var csrf_token = $('#form_buying_product [name="csrfmiddlewaretoken"]').val();
-        data["csrfmiddlewaretoken"] = csrf_token;
-        let url = form.attr("action");
-        console.log(data);
-        $.ajax({
-            url: url,
-            type: 'POST',
-            data: data,
-            cache: true,
-            success: function (data) {
-                console.log('OK');
-                console.log(data.products_total_nmb);
-                if (data.products_total_nmb)
-                    $('#basket_total_nmb').text('('+data.products_total_nmb+")");
-                    console.log(data.products);
-                    $('.basket-items ul').html("");
-                    $.each(data.products, function() {
-                        $('.basket-items ul').append('<li>'+product_name+', ' + nmb + 'шт. ' + 'по ' + product_price + ' '+ product_currency + ' ' +
-                            '<a class="delete-item">X</a>'+
-                            '<p>Всего: ' + total_price + ' ' + product_currency + '</p></li>');
-                    });
-            },
-            error: function () {
-                console.log('error')
-            }
-        });
+        basketUpdating(product_id, nmb, is_delete=false);
 
 
     });
@@ -66,12 +78,14 @@ $(document).ready(function(){
         showingBasket();
     });
 
-    // $('.basket-container').mouseout( function () {
-    //     $('.basket-items').addClass('hidden');
-    // });
+    $('.basket-container').mouseout( function () {
+        $('.basket-items').addClass('hidden');
+    });
 
     $(document).on('click', '.delete-item', function(e) {
         e.preventDefault();
-        $(this).closest('li').remove();
+        product_id = $(this).data("product_id");
+        nmb = 0;
+        basketUpdating(product_id, nmb, is_delete=true);
     });
 });
